2129601e24794f6fae934ecf1a4a5aa961295539
Clint Sullivan (vimgod) <clsulli95@outlook.com>
2024-04-14 23:24:10 -0500

    Falling sand

--- src/main.rs
+++ src/main.rs
@@ -15,20 +15,48 @@ fn config(config: GameConfig) -> GameConfig {
 fn setup(_state: &mut GameState, _c: &mut EngineContext) {
     main_camera_mut().zoom = 250.0;
     main_camera_mut().center = vec2(0.0, 0.0);
+    let dim = main_camera().world_viewport();
+    let half_x = dim.x / 2.0;
+    let half_y = dim.y / 2.0;
+    let center_x = main_camera().world_viewport().x - half_x;
+    let center_y = main_camera().world_viewport().y - half_y;
+    main_camera_mut().center = vec2(center_x, center_y);
+    println!("{}, {}", half_x, half_y);
     println!("{}", main_camera_mut().world_viewport());
+
     game_config_mut().dev.show_fps = true;
 }
 
 fn update(state: &mut GameState, _c: &mut EngineContext) {
+    let delta = delta();
     draw_light(Light::default());
 
-    if is_mouse_button_pressed(MouseButton::Left) {
-        println!("{}", mouse_world());
+    if is_mouse_button_down(MouseButton::Left) || is_mouse_button_pressed(MouseButton::Left) {
+        let mouse_pos = mouse_world();
+        let pixel_pos = vec2(mouse_pos.x.ceil(), mouse_pos.y.ceil());
+        state.world.world[pixel_pos.x as usize][pixel_pos.y as usize].p_type = PixelType::Sand;
     }
 
     for row in state.world.world.iter_mut() {
         for pixel in row.iter_mut() {
-            draw_rect(pixel.pos, vec2(1.0, 1.0), RED, 0);
+            if pixel.p_type == PixelType::Sand {
+                pixel._vel = vec2(0.0, -1.0);
+                pixel.pos.x = pixel.pos.x + pixel._vel.x;
+                pixel.pos.y = pixel.pos.y + pixel._vel.y;
+            }
+        }
+    }
+
+    for row in state.world.world.iter_mut() {
+        for pixel in row.iter_mut() {
+            let color = match pixel.p_type {
+                PixelType::Solid => RED,
+                PixelType::Sand => BEIGE,
+                PixelType::Water => BLUE,
+                PixelType::Air => TRANSPARENT,
+            };
+
+            draw_rect(pixel.pos, vec2(1.0, 1.0), color, 0);
         }
     }
 }
@@ -40,12 +68,12 @@ struct GameState {
 impl GameState {
     pub fn new(_c: &EngineState) -> Self {
         Self {
-            world: World::new(1).unwrap(),
+            world: World::new(250).unwrap(),
         }
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 enum PixelType {
     Solid,
     Sand,
@@ -56,7 +84,7 @@ enum PixelType {
 #[derive(Debug)]
 struct Pixel {
     pub pos: Vec2,
-    pub _p_type: PixelType,
+    pub p_type: PixelType,
     pub _vel: Vec2,
 }
 
@@ -64,7 +92,7 @@ impl Pixel {
     pub fn new(pos: Vec2, _vel: Vec2) -> Self {
         Self {
             pos,
-            _p_type: PixelType::Air,
+            p_type: PixelType::Air,
             _vel,
         }
     }
