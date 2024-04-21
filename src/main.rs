306bc5d9b2a781e530aa94909b9c29b19c5d0a4c
Clint Sullivan (vimgod) <clsulli95@outlook.com>
2024-04-21 13:27:40 -0500

    stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+pub mod world;
+
+use crate::world::{PixelType, World};
 use anyhow::{Context, Result};
 use comfy::*;
 use num_traits::ToPrimitive;
@@ -14,7 +17,7 @@ fn config(config: GameConfig) -> GameConfig {
 }
 
 fn setup(state: &mut GameState, _c: &mut EngineContext) {
-    main_camera_mut().zoom = 1000.0; //state.world.size as f32;
+    main_camera_mut().zoom = 250.0; //state.world.size as f32;
     main_camera_mut().center = vec2(0.0, 0.0);
     let dim = main_camera().world_viewport();
     let half_x = dim.x / 2.0;
@@ -32,6 +35,7 @@ fn update(state: &mut GameState, _c: &mut EngineContext) {
     if is_mouse_button_down(MouseButton::Left) || is_mouse_button_pressed(MouseButton::Left) {
         let mouse_pos = mouse_world() + 0.5;
         let pixel_pos = vec2(mouse_pos.x.floor(), mouse_pos.y.floor());
+        println!("{pixel_pos:?}");
         state.world.set_pixel(
             pixel_pos.x as usize,
             pixel_pos.y as usize,
@@ -63,127 +67,72 @@ trait Render {
     fn render(&self);
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum PixelType {
-    Solid,
-    Sand,
-    Water,
-    Air,
-    Border,
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum CoordSource {
-    Screen,
-    Code,
-}
-
-#[derive(Debug, Clone, Copy)]
-struct Pixel {
-    pub pos: Vec2,
-    pub p_type: PixelType,
-    pub _vel: Vec2,
-}
-
-impl Pixel {
-    pub fn new(pos: Vec2, _vel: Vec2) -> Self {
-        Self {
-            pos,
-            p_type: PixelType::Air,
-            _vel,
-        }
-    }
-}
-
-struct World {
-    pub world: Vec<Vec<Pixel>>,
-    pub size: usize,
-}
-
-impl World {
-    pub fn new(size: usize) -> Result<Self> {
-        let mut world: Vec<Vec<Pixel>> = Vec::with_capacity(size);
-
-        for row in 0..size {
-            let mut row_vec = Vec::with_capacity(size);
-            for col in 0..size {
-                let row_f32 = row.to_f32().context("usize to f32 conversion failed")?;
-                let col_f32 = col.to_f32().context("usize to f32 conversion failed")?;
-                row_vec.push(Pixel::new(vec2(row_f32, col_f32), Vec2::ZERO));
-            }
-            world.push(row_vec);
-        }
-
-        Ok(Self { world, size })
-    }
-
-    pub fn get_pixel(&self, x: usize, y: usize) -> Pixel {
-        self.world[self.size - 1 - x][y]
-    }
-
-    pub fn set_pixel(&mut self, x: usize, y: usize, t: PixelType, c: CoordSource) {
-        match c {
-            CoordSource::Screen => self.world[x][y].p_type = t,
-            CoordSource::Code => self.world[self.size - 1 - x][y].p_type = t,
-        }
-    }
-
-    pub fn get_pixel_type_below(&self, pixel: &Pixel) -> PixelType {
-        //        self.get_pixel(pixel.pos.x, pixel.pos.y - 1);
-
-        //let x = pixel.pos.y + 1 >= self.size
-        PixelType::Solid
-    }
-}
-
-impl std::fmt::Debug for World {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        for x in 0..self.size {
-            for y in 0..self.size {
-                match self.get_pixel(x, y).p_type {
-                    PixelType::Solid => write!(f, "S ")?,
-                    PixelType::Sand => write!(f, "s ")?,
-                    PixelType::Water => write!(f, "W ")?,
-                    PixelType::Air => write!(f, "A ")?,
-                    PixelType::Border => write!(f, "B ")?,
-                }
-            }
-
-            writeln!(f)?;
-        }
-
-        Ok(())
-    }
-}
+//#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+//enum PixelDirection {
+//    Down,
+//    Up,
+//    Left,
+//    Right,
+//}
+//
+//#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+//enum CoordSource {
+//    Screen,
+//    Code,
+//}
 
 impl Render for World {
     fn render(&self) {
-        for row in self.world.iter() {
-            for pixel in row.iter() {
-                let color = match pixel.p_type {
+        for x in 0..self.size {
+            for y in 0..self.size {
+                let pixel_type = self.get_pixel(x, y);
+
+                let color = match pixel_type {
                     PixelType::Solid => RED,
                     PixelType::Sand => BEIGE,
                     PixelType::Water => BLUE,
                     PixelType::Air => TRANSPARENT,
                     PixelType::Border => WHITE,
+                    PixelType::OutOfBounds => BLUE,
                 };
 
                 draw_rect(pixel.pos, vec2(1.0, 1.0), color, 0);
             }
         }
+
+            }
+        }
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+//#[cfg(test)]
+//mod tests {
+//    use super::*;
+//
+//    #[test]
+//    fn test_get_pixel_down() {
+//        let mut world = crate::World::new(50).unwrap();
+//
+//        world.set_pixel(0, 0, PixelType::Sand, CoordSource::Screen);
+//        world.set_pixel(0, 1, PixelType::Sand, CoordSource::Screen);
+//        println!("{world:?}");
+//
+//        assert_eq!(
+//            PixelType::Sand,
+//            world.get_pixel_type(0, 0, &PixelDirection::Down)
+//        );
+//    }
 
-    #[test]
-    fn test_get_pixel() {
-        let world = crate::World::new(50).unwrap();
-        assert_eq!(
-            PixelType::Solid,
-            world.get_pixel_type_below(&world.get_pixel(0, 0))
-        );
-    }
-}
+//#[test]
+//fn test_get_pixel_right() {
+//    let mut world = crate::World::new(50).unwrap();
+
+//    world.set_pixel(0, 0, PixelType::Sand, CoordSource::Code);
+//    world.set_pixel(0, 1, PixelType::Sand, CoordSource::Code);
+
+//    assert_eq!(
+//        PixelType::Sand,
+//        world.get_pixel_type(world.get_pixel(0, 0).unwrap(), &PixelDirection::Right)
+//    );
+//}
+//}
---
+++ src/world.rs
@@ -0,0 +1,122 @@
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum PixelType {
+    Solid,
+    Sand,
+    Water,
+    Air,
+    Border,
+    OutOfBounds,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Pixel {
+    pixel_type: PixelType,
+}
+
+impl Pixel {
+    pub fn new(pixel_type: PixelType) -> Self {
+        Self { pixel_type }
+    }
+}
+
+impl Default for Pixel {
+    fn default() -> Self {
+        Self {
+            pixel_type: PixelType::Solid,
+        }
+    }
+}
+
+pub struct World {
+    world: Vec<Vec<Pixel>>,
+    size: usize,
+}
+
+impl World {
+    pub fn new(size: usize) -> Self {
+        Self {
+            world: vec![vec![Pixel::default(); size]; size],
+            size,
+        }
+    }
+
+    pub fn set_pixel(&mut self, x: usize, y: usize, pixel_type: PixelType) {
+        let real_x = self.size - 1 - y;
+        let real_y = x;
+        self.world[real_x][real_y].pixel_type = pixel_type;
+    }
+
+    pub fn get_pixel(&self, x: usize, y: usize) -> PixelType {
+        let real_x = self.size - 1 - y;
+        let real_y = x;
+        self.world[real_x][real_y].pixel_type
+    }
+}
+
+impl std::fmt::Debug for World {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for (xidx, x) in self.world.iter().enumerate() {
+            for (yidx, y) in x.iter().enumerate() {
+                match y.pixel_type {
+                    PixelType::Solid => write!(f, "({},{} : S) ", xidx, yidx)?,
+                    PixelType::Sand => write!(f, "({},{} : s) ", xidx, yidx)?,
+                    PixelType::Water => write!(f, "({},{} : W) ", xidx, yidx)?,
+                    PixelType::Air => write!(f, "({},{} : A) ", xidx, yidx)?,
+                    PixelType::Border => write!(f, "({},{} : B) ", xidx, yidx)?,
+                    PixelType::OutOfBounds => write!(f, "({},{} : O) ", xidx, yidx)?,
+                }
+            }
+
+            writeln!(f)?;
+        }
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new_pixel_type() {
+        let pixel = Pixel::new(PixelType::Solid);
+        assert_eq!(PixelType::Solid, pixel.pixel_type);
+        let pixel = Pixel::new(PixelType::Sand);
+        assert_eq!(PixelType::Sand, pixel.pixel_type);
+        let pixel = Pixel::new(PixelType::Water);
+        assert_eq!(PixelType::Water, pixel.pixel_type);
+        let pixel = Pixel::new(PixelType::Air);
+        assert_eq!(PixelType::Air, pixel.pixel_type);
+        let pixel = Pixel::new(PixelType::Border);
+        assert_eq!(PixelType::Border, pixel.pixel_type);
+        let pixel = Pixel::new(PixelType::OutOfBounds);
+        assert_eq!(PixelType::OutOfBounds, pixel.pixel_type);
+    }
+
+    #[test]
+    fn test_get_pixel() {
+        let mut world = World::new(4);
+        world.world[0][0].pixel_type = PixelType::Water;
+        world.world[1][1].pixel_type = PixelType::Water;
+        world.world[2][2].pixel_type = PixelType::Water;
+        world.world[3][3].pixel_type = PixelType::Water;
+        assert_eq!(PixelType::Water, world.get_pixel(0, 3));
+        assert_eq!(PixelType::Water, world.get_pixel(1, 2));
+        assert_eq!(PixelType::Water, world.get_pixel(2, 1));
+        assert_eq!(PixelType::Water, world.get_pixel(3, 0));
+    }
+
+    #[test]
+    fn test_set_pixel() {
+        let mut world = World::new(4);
+        world.set_pixel(0, 3, PixelType::Water);
+        world.set_pixel(1, 2, PixelType::Water);
+        world.set_pixel(2, 1, PixelType::Water);
+        world.set_pixel(3, 0, PixelType::Water);
+        assert_eq!(PixelType::Water, world.world[0][0].pixel_type);
+        assert_eq!(PixelType::Water, world.world[1][1].pixel_type);
+        assert_eq!(PixelType::Water, world.world[2][2].pixel_type);
+        assert_eq!(PixelType::Water, world.world[3][3].pixel_type);
+    }
+}
