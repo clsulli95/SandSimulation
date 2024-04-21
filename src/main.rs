75f2cbef9dc98cb8948bc8c381c405664aa9147c
Clint Sullivan (vimgod) <clsulli95@outlook.com>
2024-04-21 16:09:15 -0500

    Pixel Adjacency working

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,73 @@
 pub mod world;
 
-use crate::world::{PixelType, World};
-use anyhow::{Context, Result};
-use comfy::*;
-use num_traits::ToPrimitive;
-use std::io::{stdout, Write};
+//use crate::world::{PixelType, World};
+//use anyhow::{Context, Result};
+//use comfy::*;
+//use num_traits::ToPrimitive;
+//use std::io::{stdout, Write};
 
-simple_game!("My Game Demo", GameState, config, setup, update);
+fn main() {}
 
-fn config(config: GameConfig) -> GameConfig {
-    GameConfig {
-        vsync_enabled: false,
-        target_framerate: 60,
-        ..config
-    }
-}
-
-fn setup(state: &mut GameState, _c: &mut EngineContext) {
-    main_camera_mut().zoom = 250.0; //state.world.size as f32;
-    main_camera_mut().center = vec2(0.0, 0.0);
-    let dim = main_camera().world_viewport();
-    let half_x = dim.x / 2.0;
-    let half_y = dim.y / 2.0;
-    let center_x = main_camera().world_viewport().x - half_x;
-    let center_y = main_camera().world_viewport().y - half_y;
-    main_camera_mut().center = vec2(center_x, center_y);
-    game_config_mut().dev.show_fps = true;
-}
-
-fn update(state: &mut GameState, _c: &mut EngineContext) {
-    let delta = delta();
-    draw_light(Light::default());
-
-    if is_mouse_button_down(MouseButton::Left) || is_mouse_button_pressed(MouseButton::Left) {
-        let mouse_pos = mouse_world() + 0.5;
-        let pixel_pos = vec2(mouse_pos.x.floor(), mouse_pos.y.floor());
-        println!("{pixel_pos:?}");
-        state.world.set_pixel(
-            pixel_pos.x as usize,
-            pixel_pos.y as usize,
-            PixelType::Sand,
-            CoordSource::Screen,
-        );
-    }
-
-    if is_mouse_button_pressed(MouseButton::Right) {
-        println!("{:?}", state.world);
-    }
-
-    state.world.render();
-}
-
-struct GameState {
-    pub world: World,
-}
-
-impl GameState {
-    pub fn new(_c: &EngineState) -> Self {
-        Self {
-            world: World::new(250).unwrap(),
-        }
-    }
-}
-
-trait Render {
-    fn render(&self);
-}
+//simple_game!("My Game Demo", GameState, config, setup, update);
+//
+//fn config(config: GameConfig) -> GameConfig {
+//    GameConfig {
+//        vsync_enabled: false,
+//        target_framerate: 60,
+//        ..config
+//    }
+//}
+//
+//fn setup(state: &mut GameState, _c: &mut EngineContext) {
+//    main_camera_mut().zoom = 250.0; //state.world.size as f32;
+//    main_camera_mut().center = vec2(0.0, 0.0);
+//    let dim = main_camera().world_viewport();
+//    let half_x = dim.x / 2.0;
+//    let half_y = dim.y / 2.0;
+//    let center_x = main_camera().world_viewport().x - half_x;
+//    let center_y = main_camera().world_viewport().y - half_y;
+//    main_camera_mut().center = vec2(center_x, center_y);
+//    game_config_mut().dev.show_fps = true;
+//}
+//
+//fn update(state: &mut GameState, _c: &mut EngineContext) {
+//    let delta = delta();
+//    draw_light(Light::default());
+//
+//    if is_mouse_button_down(MouseButton::Left) || is_mouse_button_pressed(MouseButton::Left) {
+//        let mouse_pos = mouse_world() + 0.5;
+//        let pixel_pos = vec2(mouse_pos.x.floor(), mouse_pos.y.floor());
+//        println!("{pixel_pos:?}");
+//        state.world.set_pixel(
+//            pixel_pos.x as usize,
+//            pixel_pos.y as usize,
+//            PixelType::Sand,
+//            CoordSource::Screen,
+//        );
+//    }
+//
+//    if is_mouse_button_pressed(MouseButton::Right) {
+//        println!("{:?}", state.world);
+//    }
+//
+//    state.world.render();
+//}
+//
+//struct GameState {
+//    pub world: World,
+//}
+//
+//impl GameState {
+//    pub fn new(_c: &EngineState) -> Self {
+//        Self {
+//            world: World::new(250).unwrap(),
+//        }
+//    }
+//}
+//
+//trait Render {
+//    fn render(&self);
+//}
 
 //#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 //enum PixelDirection {
@@ -81,29 +83,29 @@ trait Render {
 //    Code,
 //}
 
-impl Render for World {
-    fn render(&self) {
-        for x in 0..self.size {
-            for y in 0..self.size {
-                let pixel_type = self.get_pixel(x, y);
-
-                let color = match pixel_type {
-                    PixelType::Solid => RED,
-                    PixelType::Sand => BEIGE,
-                    PixelType::Water => BLUE,
-                    PixelType::Air => TRANSPARENT,
-                    PixelType::Border => WHITE,
-                    PixelType::OutOfBounds => BLUE,
-                };
-
-                draw_rect(pixel.pos, vec2(1.0, 1.0), color, 0);
-            }
-        }
-
-            }
-        }
-    }
-}
+//impl Render for World {
+//    fn render(&self) {
+//        for x in 0..self.size {
+//            for y in 0..self.size {
+//                let pixel_type = self.get_pixel(x, y);
+//
+//                let color = match pixel_type {
+//                    PixelType::Solid => RED,
+//                    PixelType::Sand => BEIGE,
+//                    PixelType::Water => BLUE,
+//                    PixelType::Air => TRANSPARENT,
+//                    PixelType::Border => WHITE,
+//                    PixelType::OutOfBounds => BLUE,
+//                };
+//
+//                draw_rect(pixel.pos, vec2(1.0, 1.0), color, 0);
+//            }
+//        }
+//
+//            }
+//        }
+//    }
+//}
 
 //#[cfg(test)]
 //mod tests {
--- src/world.rs
+++ src/world.rs
@@ -1,3 +1,5 @@
+use anyhow::{anyhow, Result};
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum PixelType {
     Solid,
@@ -29,14 +31,14 @@ impl Default for Pixel {
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Adjacency {
-    TOP_LEFT,
-    TOP,
-    TOP_RIGHT,
-    MID_LEFT,
-    MID_RIGHT,
-    BOT_LEFT,
-    BOT,
-    BOT_RIGHT,
+    AboveLeft,
+    Above,
+    AboveRight,
+    Left,
+    Right,
+    BelowLeft,
+    Below,
+    BelowRight,
 }
 
 pub struct World {
@@ -52,43 +54,68 @@ impl World {
         }
     }
 
-    pub fn set_pixel(&mut self, x: usize, y: usize, pixel_type: PixelType) {
+    pub fn set_pixel(&mut self, x: usize, y: usize, pixel_type: PixelType) -> Result<()> {
         let real_x = self.size - 1 - y;
         let real_y = x;
-        self.world[real_x][real_y].pixel_type = pixel_type;
+        self.world
+            .get_mut(real_x)
+            .and_then(|y_vec| y_vec.get_mut(real_y))
+            .map(|pixel| pixel.pixel_type = pixel_type)
+            .ok_or(anyhow!("Out of Bounds Access"))
+        //self.world[real_x][real_y].pixel_type = pixel_type;
     }
 
     pub fn get_pixel(&self, x: usize, y: usize) -> PixelType {
         let real_x = self.size - 1 - y;
         let real_y = x;
-        self.world[real_x][real_y].pixel_type
+        self.world
+            .get(real_x)
+            .and_then(|y_vec| y_vec.get(real_y))
+            .map_or(PixelType::OutOfBounds, |pixel| pixel.pixel_type)
     }
 
     pub fn get_pixel_adj(&self, x: usize, y: usize, adj: Adjacency) -> PixelType {
-        let mut real_x = self.size - 1 - y;
-        let mut real_y = x;
-
+        let mut lookup_x = x;
+        let mut lookup_y = y;
         match adj {
-            Adjacency::TOP_LEFT => {
-                real_x -= 1;
+            Adjacency::AboveLeft => {
+                lookup_x -= 1;
+                lookup_y += 1;
             }
-            Adjacency::TOP => real_y += 1,
-            Adjacency::TOP_RIGHT => {
-                real_x += 1;
-                real_y += 1;
+            Adjacency::Above => {
+                lookup_y += 1;
             }
-            Adjacency::MID_LEFT => real_x -= 1,
-            Adjacency::MID_RIGHT => real_x += 1,
-            Adjacency::BOT_LEFT => {
-                real_x -= 1;
-                real_y -= 1;
+            Adjacency::AboveRight => {
+                lookup_x += 1;
+                lookup_y += 1;
             }
-            Adjacency::BOT => real_y - 1,
-            Adjacency::BOT_RIGHT => {
-                real_x += 1;
-                real_y -= 1;
+            Adjacency::Left => {
+                lookup_x -= 1;
+            }
+            Adjacency::Right => {
+                lookup_x += 1;
+            }
+            Adjacency::BelowLeft => {
+                lookup_x -= 1;
+                lookup_y -= 1;
+            }
+            Adjacency::Below => {
+                lookup_y -= 1;
+            }
+
+            Adjacency::BelowRight => {
+                lookup_x += 1;
+                lookup_y -= 1;
             }
         }
+
+        let real_x = self.size - 1 - lookup_y;
+        let real_y = lookup_x;
+
+        self.world
+            .get(real_x)
+            .and_then(|y_vec| y_vec.get(real_y))
+            .map_or(PixelType::OutOfBounds, |pixel| pixel.pixel_type)
     }
 }
 
@@ -149,13 +176,97 @@ mod tests {
     #[test]
     fn test_set_pixel() {
         let mut world = World::new(4);
-        world.set_pixel(0, 3, PixelType::Water);
-        world.set_pixel(1, 2, PixelType::Water);
-        world.set_pixel(2, 1, PixelType::Water);
-        world.set_pixel(3, 0, PixelType::Water);
+        world.set_pixel(0, 3, PixelType::Water).unwrap();
+        world.set_pixel(1, 2, PixelType::Water).unwrap();
+        world.set_pixel(2, 1, PixelType::Water).unwrap();
+        world.set_pixel(3, 0, PixelType::Water).unwrap();
         assert_eq!(PixelType::Water, world.world[0][0].pixel_type);
         assert_eq!(PixelType::Water, world.world[1][1].pixel_type);
         assert_eq!(PixelType::Water, world.world[2][2].pixel_type);
         assert_eq!(PixelType::Water, world.world[3][3].pixel_type);
+    }
+
+    #[test]
+    fn test_get_pixel_adj_above_left() {
+        let mut world = World::new(4);
+        world.set_pixel(1, 3, PixelType::Sand).unwrap();
+        world.set_pixel(2, 2, PixelType::Water).unwrap();
+
+        assert_eq!(
+            PixelType::Sand,
+            world.get_pixel_adj(2, 2, Adjacency::AboveLeft)
+        );
+    }
+
+    #[test]
+    fn test_get_pixel_adj_above() {
+        let mut world = World::new(4);
+        world.set_pixel(2, 3, PixelType::Sand).unwrap();
+        world.set_pixel(2, 2, PixelType::Water).unwrap();
+
+        assert_eq!(PixelType::Sand, world.get_pixel_adj(2, 2, Adjacency::Above));
+    }
+
+    #[test]
+    fn test_get_pixel_adj_left() {
+        let mut world = World::new(4);
+        world.set_pixel(1, 2, PixelType::Sand).unwrap();
+        world.set_pixel(2, 2, PixelType::Water).unwrap();
+
+        assert_eq!(PixelType::Sand, world.get_pixel_adj(2, 2, Adjacency::Left));
+    }
+
+    #[test]
+    fn test_get_pixel_adj_right() {
+        let mut world = World::new(4);
+        world.set_pixel(3, 2, PixelType::Sand).unwrap();
+        world.set_pixel(2, 2, PixelType::Water).unwrap();
+
+        assert_eq!(PixelType::Sand, world.get_pixel_adj(2, 2, Adjacency::Right));
+    }
+
+    #[test]
+    fn test_get_pixel_adj_above_right() {
+        let mut world = World::new(4);
+        world.set_pixel(3, 3, PixelType::Sand).unwrap();
+        world.set_pixel(2, 2, PixelType::Water).unwrap();
+
+        assert_eq!(
+            PixelType::Sand,
+            world.get_pixel_adj(2, 2, Adjacency::AboveRight)
+        );
+    }
+
+    #[test]
+    fn test_get_pixel_adj_below_left() {
+        let mut world = World::new(4);
+        world.set_pixel(1, 1, PixelType::Sand).unwrap();
+        world.set_pixel(2, 2, PixelType::Water).unwrap();
+
+        assert_eq!(
+            PixelType::Sand,
+            world.get_pixel_adj(2, 2, Adjacency::BelowLeft)
+        );
+    }
+
+    #[test]
+    fn test_get_pixel_adj_below() {
+        let mut world = World::new(4);
+        world.set_pixel(2, 1, PixelType::Sand).unwrap();
+        world.set_pixel(2, 2, PixelType::Water).unwrap();
+
+        assert_eq!(PixelType::Sand, world.get_pixel_adj(2, 2, Adjacency::Below));
+    }
+
+    #[test]
+    fn test_get_pixel_adj_below_right() {
+        let mut world = World::new(4);
+        world.set_pixel(3, 1, PixelType::Sand).unwrap();
+        world.set_pixel(2, 2, PixelType::Water).unwrap();
+
+        assert_eq!(
+            PixelType::Sand,
+            world.get_pixel_adj(2, 2, Adjacency::BelowRight)
+        );
     }
 }
